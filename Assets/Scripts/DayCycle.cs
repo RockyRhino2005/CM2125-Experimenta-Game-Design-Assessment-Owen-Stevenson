ea6420eb9a33e3a4858fb523b4df996b44d8d7c5
RockyRhino2005 <owenstevenson@hotmail.co.uk>
2024-10-31 10:00:53 +0000

    Added New Sprites and started working with day cycle time

---
+++ Assets/Scripts/DayCycle.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DayCycle : MonoBehaviour
+{
+
+    // for day/time display
+    public TextMeshProUGUI timeDisplay;
+    public TextMeshProUGUI dayDisplay;
+
+    public float seconds;
+    public float minutes;
+    public float hours;
+    public float days;
+    public float tick;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate(){
+
+        calcTime();
+        DisplayTime();
+    }
+
+    public void calcTime(){
+
+        seconds += Time.fixedDeltaTime * tick;
+
+        // resets seconds every 60 ingame and updates minutes accordingly
+        if (seconds >= 60){
+            seconds = 0;
+            minutes += 1;
+        }
+
+        // resets minutes every 60 ingame and updates hours accordingly
+        if (seconds >= 60){
+            minutes = 0;
+            hours += 1;
+        }
+
+        // resets hours every 24 and updates days accordingly
+        if (hours >= 24){
+            hours = 0;
+            days += 1;
+        }
+
+    }
+
+    public void DisplayTime(){
+        // format makes there always be 0s so the text is never empty
+        timeDisplay.text = string.Format("[0.00].[1.00]", hours, minutes); 
+        dayDisplay.text = "Day: " + days;
+
+    }
+}
