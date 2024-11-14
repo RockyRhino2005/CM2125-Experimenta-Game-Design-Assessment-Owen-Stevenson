1496142476d0c48a1a34c45a5e6d4f81a3711f79
RockyRhino2005 <owenstevenson@hotmail.co.uk>
2024-11-14 10:15:05 +0000

    merged code as they relied on each other

--- Assets/Scripts/door1.cs
+++ Assets/Scripts/door1.cs
@@ -6,14 +6,18 @@ using UnityEngine.UI;
 // script contains door mechanics
 // script contains day cycle mechanics
 // script contains timer mechanics
-public class door1 : MonoBehaviour
+public class Door1 : MonoBehaviour
 {
     // time
     public float seconds;
     public float minutes;
     public float hours;
     public float days;
-    public float timer = 30;
+
+    // timer
+    private float timer = 60;
+    private bool area = false;
+
     // day cycle
     public float cycle;
     private bool startDay;
@@ -23,7 +27,9 @@ public class door1 : MonoBehaviour
             CalcTime();
         }
 
-
+        if (area == true){
+            StartTimer();
+        }
     }
 
     public void CalcTime(){
@@ -50,19 +56,19 @@ public class door1 : MonoBehaviour
         if(hours == 5.0){
             cycle = 2;
             hours += 1;
-            changeTime();
+            ChangeTime();
         }
 
         if(hours == 11.0){
             cycle = 3;
             hours += 1;
-            changeTime();
+            ChangeTime();
         }
 
         if(hours == 17.0){
             cycle = 1;
             hours += 1;
-            changeTime();
+            ChangeTime();
         }
 
         // resets hours every 24 and updates days accordingly
@@ -74,24 +80,61 @@ public class door1 : MonoBehaviour
 
 
     }
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     // when player touches the door they are transported to the next section
     public void OnCollisionEnter2D(Collision2D collision){
-        if (collision.gameObject.tag == "door1"){
-            UnityEngine.Debug.Log("changed location");
+        // first door section
+        if (collision.gameObject.CompareTag("door1")){
+            Debug.Log("changed location");
             transform.position=new Vector3(transform.position.x-310,transform.position.y-60, transform.position.z);
             startDay = true;
+            area = true;
+            Debug.Log("timer started");
             Debug.Log("daylight cycle Started");
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision){
+
+        // door 2
+        // ends timer
+        if (collision.gameObject.CompareTag("door2")){
+            Debug.Log("changed location");
+            startDay = false;
+            area = false;
+            Debug.Log("You outran the clock");
+
+            if (cycle == 2){
+                transform.position=new Vector3(transform.position.x,transform.position.y-280, transform.position.z);
+            }
+            // teleport to afternoon
+            if (cycle == 3){
+                transform.position=new Vector3(transform.position.x,transform.position.y-240, transform.position.z);
+            } 
+            // teleport to night
+            else if (cycle == 1) {
+                transform.position=new Vector3(transform.position.x,transform.position.y-200, transform.position.z);
+            }
         }
 
     }
 
-    public void changeTime(){
+    // timer that counts down once youve entered the outside area
+    // if it hits 0 you die and the level resets
+    public void StartTimer(){
+        // decreases timer each second
+        timer -= (Time.deltaTime);
+        // displays timer to debug log
+        Debug.Log(timer);
+
+        // after 30 seconds timer explodes and you die
+        if(timer < 0){
+            Debug.Log("The Timer Exploded");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    public void ChangeTime(){
         // teleport to day
         if (cycle == 2){
 
@@ -106,4 +149,6 @@ public class door1 : MonoBehaviour
             transform.position=new Vector3(transform.position.x,transform.position.y+100, transform.position.z);
         }
     }
+
+    
 }
